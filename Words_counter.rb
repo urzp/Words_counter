59228b99891acce175fbc7fbf3d8493136f13f80
urzp <urzp002@mail.ru>
2016-05-18 18:22:57 +0300

    Put comment into *.rb

--- Words_counter.rb
+++ Words_counter.rb
@@ -4,4 +4,4 @@ words=text.split(" ")
 frequencies=Hash.new(0)
 
 frequencies=Hash.new(0)
-words.each{|i| frequencies[i]+=1 }
+words.each{|i| frequencies[i]+=1 } # помешаем в хеш слова (i) если встречаеться это словво прибовляем  frequencies[i]+=1

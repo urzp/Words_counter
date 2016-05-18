8061e4c6d47542d46dc93aca41b277b459ba7975
urzp <urzp002@mail.ru>
2016-05-18 18:29:30 +0300

    Sort by repits words

--- Words_counter.rb
+++ Words_counter.rb
@@ -5,3 +5,6 @@ frequencies=Hash.new(0)
 
 frequencies=Hash.new(0)
 words.each{|i| frequencies[i]+=1 } # помешаем в хеш слова (i) если встречаеться это словво прибовляем  frequencies[i]+=1
+
+frequencies=frequencies.sort_by{|word,count| count}  # сортеруем слова по колличеству повторений и переворачивам хеш по убывании колл повторений
+frequencies.reverse!
